59967b5f7a7327d58e5f1ddb68df2b7d83f28b82
Yann Jorelle <yannjorelle@proton.me>
2023-07-12 15:39:00 +0300

    10.12: environment variables

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
-  uri: 'http://10.134.24.21:4000/graphql',
+  uri: Constants.manifest?.extra?.apolloUri,
 });
 
 const createApolloClient = () => {
